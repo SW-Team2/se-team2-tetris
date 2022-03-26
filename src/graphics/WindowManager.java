d599593767064eb01238614c7dac13ddbd3e4dab
siyeong <synnn0910@gmail.com>
2022-03-26 22:08:32 +0900

    feature: show(String)메서드 추가, 윈도우를 닫을때 행동 정의

--- src/graphics/WindowManager.java
+++ src/graphics/WindowManager.java
@@ -3,8 +3,10 @@ package graphics;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
 
 import java.awt.*;
 
@@ -22,36 +24,42 @@ public class WindowManager {
 
     private WindowManager() {
         mWindow = new JFrame();
+
         mWindow.setSize(800, 600);
         mWindow.setLocation(200, 200);
         mCards = new CardLayout(0, 0);
         mWindow.getContentPane().setLayout(mCards);
+
+        mWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         MakeMainMenu();
         MakeGameScreen();
         MakeSettingMenu();
         MakeScoreBoard();
 
-        mWindow.getContentPane().add("Main", mMainMenu);
-        mWindow.getContentPane().add("Game", mGameScreen);
-        mWindow.getContentPane().add("Setting", mSettingMenu);
-        mWindow.getContentPane().add("Score", mScoreBoard);
+        mWindow.getContentPane().add("main", mMainMenu);
+        mWindow.getContentPane().add("game", mGameScreen);
+        mWindow.getContentPane().add("setting", mSettingMenu);
+        mWindow.getContentPane().add("score", mScoreBoard);
 
-        class Input implements KeyListener {
+        //
+        // Define action when keybaord message occurs
+        //
+        mWindow.addKeyListener(new KeyListener() {
             @Override
             public void keyPressed(KeyEvent e) {
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_Q:
-                        mCards.show(mWindow.getContentPane(), "Main");
+                        mCards.show(mWindow.getContentPane(), "main");
                         break;
                     case KeyEvent.VK_W:
-                        mCards.show(mWindow.getContentPane(), "Game");
+                        mCards.show(mWindow.getContentPane(), "game");
                         break;
                     case KeyEvent.VK_E:
-                        mCards.show(mWindow.getContentPane(), "Setting");
+                        mCards.show(mWindow.getContentPane(), "setting");
                         break;
                     case KeyEvent.VK_R:
-                        mCards.show(mWindow.getContentPane(), "Score");
+                        mCards.show(mWindow.getContentPane(), "score");
                         break;
                     default:
                         break;
@@ -65,8 +73,22 @@ public class WindowManager {
             @Override
             public void keyTyped(KeyEvent e) {
             }
-        }
-        mWindow.addKeyListener(new Input());
+        });
+
+        //
+        // Define action when closing window message occurs
+        //
+        mWindow.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                if (JOptionPane.showConfirmDialog(mWindow,
+                        "Are you sure you want to close this window?", "Close Window?",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+                    System.exit(0);
+                }
+            }
+        });
 
         mWindow.setVisible(true);
     }
@@ -76,6 +98,10 @@ public class WindowManager {
             mUniqueInstance = new WindowManager();
         }
         return mUniqueInstance;
+    }
+
+    public void show(String name) {
+        mCards.show(mWindow.getContentPane(), name);
     }
 
     private void MakeMainMenu() {
